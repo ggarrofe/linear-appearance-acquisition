17eca2d0542567a5c1d671cf7941534b4c04c5c8
Guillem Garrofe <guillemgarrofemontoliu@gmail.com>
2022-06-14 16:21:56 +0200

    infinite loss, have to compute the loss per batch
    Former-commit-id: 60e2f969c2b311bed4eb256a69c5220430f8e8c2

--- NeRF/utils/utils.py
+++ NeRF/utils/utils.py
@@ -10,11 +10,12 @@ def has_nan(tensor):
     return tensor.isnan().any()
 
 def summarize_diff(old_arr, new_arr):
+    assert(old_arr.shape == new_arr.shape, "The 2 arrays have different shape, cannot find the differences.")
+    
     total_items = reduce(lambda x, y: x*y, list(new_arr.shape))
     diff_items = np.where(old_arr != new_arr)
-    print("diff_items", diff_items[0].shape, diff_items[1].shape, new_arr.shape)
     num_diff = np.count_nonzero(old_arr != new_arr)
 
-    print(f"Changed {num_diff} items out of {total_items}")
+    print(f"Changed {num_diff} items out of {total_items} in array of shape {new_arr.shape}")
     if num_diff > 0:
-        print(f"\tFrom ({diff_items[0][0]}, {diff_items[0][0]}) to ({diff_items[-1][0]}, {diff_items[-1][0]})")
+        print(f"\tFrom ({diff_items[0][0]}, {diff_items[1][0]}, {diff_items[2][0]}) to ({diff_items[0][-1]}, {diff_items[1][-1]}, {diff_items[2][-1]})")
