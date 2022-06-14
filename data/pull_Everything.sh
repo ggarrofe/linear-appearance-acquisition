f4f85a3b47b51dff1d884337178654b73682ff39
Guillem Garrofe <guillemgarrofemontoliu@gmail.com>
2022-06-14 16:22:08 +0200

    minor changes
    Former-commit-id: b7fdb54b3f664e41110753832c8698db5550142c

--- data/pull_Everything.sh
+++ data/pull_Everything.sh
@@ -1,3 +1,5 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:28f5f03b83d3379ebbdb41db544cfbea4f9de230e9b340d1313aad14dcdd54d6
-size 58
+for dir in ./*; do
+    cd $dir
+    git pull
+    cd ..
+done
