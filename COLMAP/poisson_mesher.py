9cfd4c4b0968351f7579d355322332d75e492b54
Guillem Garrofe <guillemgarrofemontoliu@gmail.com>
2022-06-29 21:34:59 +0100

    minor changes
    Former-commit-id: 462c8fc76d78cbb71eadfa15f2af606167b1ea90

--- COLMAP/poisson_mesher.py
+++ COLMAP/poisson_mesher.py
@@ -1,5 +1,7 @@
 import open3d as o3d
 import sys
+import numpy as np
+import matplotlib.pyplot as plt
 
 # usage python3 poisson_mesher.py ./lego_llff/dense/fused.ply ./lego_llff/dense/meshed-poisson.ply
 if __name__ == "__main__":
@@ -7,5 +9,22 @@ if __name__ == "__main__":
 
     with o3d.utility.VerbosityContextManager(o3d.utility.VerbosityLevel.Debug) as cm:
         mesh, densities = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(pcd, depth=9)
-    
+
+    o3d.visualization.draw_geometries([mesh])
+
+    densities = np.asarray(densities)
+    density_colors = plt.get_cmap('plasma')(
+        (densities - densities.min()) / (densities.max() - densities.min()))
+    density_colors = density_colors[:, :3]
+    density_mesh = o3d.geometry.TriangleMesh()
+    density_mesh.vertices = mesh.vertices
+    density_mesh.triangles = mesh.triangles
+    density_mesh.triangle_normals = mesh.triangle_normals
+    density_mesh.vertex_colors = o3d.utility.Vector3dVector(density_colors)
+    o3d.visualization.draw_geometries([density_mesh])
+
+    vertices_to_remove = densities < np.quantile(densities, 0.01)
+    mesh.remove_vertices_by_mask(vertices_to_remove)
+    o3d.visualization.draw_geometries([mesh])
+
     o3d.io.write_triangle_mesh(sys.argv[2], mesh)
