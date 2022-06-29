a4061f38887b0c53adb7bf79e088b2868be81d2c
Guillem Garrofe <guillemgarrofemontoliu@gmail.com>
2022-06-29 18:19:39 +0100

    poisson mesher
    Former-commit-id: a7718b4c7dc3b2b08de81e5b5940195dd5f3b21c

--- COLMAP/poisson_mesher.py
+++ COLMAP/poisson_mesher.py
@@ -1,11 +1,11 @@
 import open3d as o3d
+import sys
 
+# usage python3 poisson_mesher.py ./lego_llff/dense/fused.ply ./lego_llff/dense/meshed-poisson.ply
 if __name__ == "__main__":
-    pcd = o3d.io.read_point_cloud()
+    pcd = o3d.io.read_point_cloud(sys.argv[1])
 
-    with o3d.utility.VerbosityContextManager(
-            o3d.utility.VerbosityLevel.Debug) as cm:
-        mesh, densities = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(
-            pcd, depth=9)
-    print(mesh)
-    o3d.visualization.draw_geometries([mesh])
+    with o3d.utility.VerbosityContextManager(o3d.utility.VerbosityLevel.Debug) as cm:
+        mesh, densities = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(pcd, depth=9)
+    
+    o3d.io.write_triangle_mesh(sys.argv[2], mesh)
